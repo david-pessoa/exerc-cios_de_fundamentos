d509075fc8f83877ab4e6dc451f916dc75115223
david-pessoa <104323068+david-pessoa@users.noreply.github.com>
2022-05-13 21:22:19 -0300

    Add files via upload

---
+++ matriz desenho.py
@@ -0,0 +1,31 @@
+###################  Exercício 5  #################
+bits = [[0,0,0,0],[0,0,1,0],[0,0,1,0],[1,0,0,1]]
+desenho = ""
+for i in range(len(bits)):
+    for j in range(len(bits[i])):
+        if bits[i][j] == 0:
+            desenho += " "
+        else:
+            desenho += "*"
+    desenho += "\n"
+print(desenho)
+        
+###################  Exercício 6  #################
+bits = [[0,0,0,0],[0,0,1,0],[0,0,1,0],[1,0,0,1]]
+desenho = ""
+for i in range(len(bits)):
+    for j in range(len(bits[i])):
+        if bits[i][j] == 0:
+            desenho += " "
+        else:
+            desenho += "*"
+    desenho += "\n"
+print(desenho)
+        
+    
+    
+
+
+
+
+        
